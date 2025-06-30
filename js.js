5f16e84cafc7012c8b0bc8bbd9412c18cac5aed4
Cole Clarkson <rockhoundcole@gmail.com>
2025-06-30 19:58:30 -0400

    a

---
+++ js.js
@@ -0,0 +1,15 @@
+function person(name,tg,yearsEXP,edu,skills,certif,lanspeek,url,lokat,remote,soft,intern){
+    this.name = name;
+    this.modle = tg;
+    this.yearsEXP = yearsEXP;
+    this.edu = edu,
+    this.skills = skills,
+    this.certif = certif,
+    this.lanspeek = lanspeek,
+    this.url = url,
+    this.lokat = lokat, 
+    this.remote = remote,
+    this.soft = soft,
+    this.intern = intern
+
+}
