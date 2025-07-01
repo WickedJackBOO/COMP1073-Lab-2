2a7498cae8b68c4622cbe52f928f5d3fbd5ffdb7
Cole Clarkson <rockhoundcole@gmail.com>
2025-06-30 20:03:53 -0400

    b

--- js.js
+++ js.js
@@ -1,15 +1,31 @@
-function person(name,tg,yearsEXP,edu,skills,certif,lanspeek,url,lokat,remote,soft,intern){
+function person(name,locat,tg,yearsEXP,edu,skills,certif,lanspeek,url,relokat,remote,soft,intern){
     this.name = name;
-    this.modle = tg;
+    this.locat =locat;
+    this.tg = tg;
     this.yearsEXP = yearsEXP;
     this.edu = edu,
     this.skills = skills,
     this.certif = certif,
     this.lanspeek = lanspeek,
     this.url = url,
-    this.lokat = lokat, 
+    this.relokat = relokat, 
     this.remote = remote,
     this.soft = soft,
     this.intern = intern
-
 }
+
+let app1 = new person(
+    "Liam O'Reilly",
+    "Halifax, NS",
+    "Marine Technician",
+    4,
+    "Diploma in Marine Technology, Nova Scotia Community College",
+    ["Sonar operation", "Engine repair", "Navigation systems"],
+    ["Marine Safety Training"],
+    ["English"],
+    null,
+    true,
+    false,
+    ["Teamwork", "Adaptability"],
+    false
+);
